0b4ec626861fc392d740d110eed658525790885d
Yamine Bulach <yamine.bulach@fullsave.com>
2018-08-31 15:16:09 +0200

    added walls

--- gamejam/map.py
+++ gamejam/map.py
@@ -20,14 +20,21 @@ class Map(object):
         """
         self._walls = [
             # racks room
-            Wall(0, 48, 32, 8),
-            Wall(48, 48, 64, 8),
-            Wall(112, 0, 8, 64),
+            Wall(0, 128, 32, 8),
+            Wall(64, 128, 96, 8),
+            Wall(160, 0, 8, 136),
 
             # providers room
-            Wall(0, 96, 64, 8),
+            Wall(0, 160, 80, 8),
+            Wall(112, 160, 8, 72),
 
             # table room
+            Wall(154, 160, 56, 8),
+            Wall(210, 160, 8, 72),
+
+            # network room
+            Wall(208, 128, 48, 8),
+            Wall(200, 32, 8, 104),
         ]
         self._racks = []
         self._providers = []
