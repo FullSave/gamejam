206c7ed6165e82aba33d8aa796cc91ae8568058c
Fabien Dupont <fabien.dupont@fullsave.com>
2018-08-31 10:36:48 +0200

    Moved Element and Spritesheet to misc.py

---
+++ gamejam/misc.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+This file is part of FullSave Gamejam.
+Copyrights 2018 by Fullsave
+"""
+
+import pyxel
+
+
+class SpriteSheet(object):
+    def __init__(self, image, path, w, h, mask=0):
+        self._image = image
+        self.w = w
+        self.h = h
+        self._mask = mask
+
+    def render(self, x, y, mask=None):
+        if mask is None:
+            mask = self._mask
+
+        return [self._image, self._w * x, self._h * y, self._h, self._w, mask]
+
+
+class Element(object):
+    def __init__(self, x, y, w, h, spritesheet, sx, sy):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.spritesheet = spritesheet
+        self.sprite = spritesheet.render(sx, sy)
+
+    def draw(self):
+        pyxel.blt(self.x, self.y, *self.sprite)
