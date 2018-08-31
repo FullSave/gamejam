d96c34de6161882f8017395e2214caa21f6868f3
Fabien Dupont <fabien.dupont@fullsave.com>
2018-08-31 11:06:43 +0200

    Misc classes for game elements

--- gamejam/misc.py
+++ gamejam/misc.py
@@ -9,6 +9,22 @@ Copyrights 2018 by Fullsave
 import pyxel
 
 
+class Hitbox(object):
+    def __init__(self, x, y, w, h):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+
+    @property
+    def x2(self):
+        return self.x + self.w
+
+    @property
+    def y2(self):
+        return self.y + self.h
+
+
 class SpriteSheet(object):
     def __init__(self, image, path, w, h, mask=0):
         self._image = image
@@ -20,17 +36,46 @@ class SpriteSheet(object):
         if mask is None:
             mask = self._mask
 
-        return [self._image, self._w * x, self._h * y, self._h, self._w, mask]
+        return self._image, self._w * x, self._h * y, self._h, self._w, mask
 
 
 class Element(object):
     def __init__(self, x, y, w, h, spritesheet, sx, sy):
+        """Basic Game Element
+
+        Arguments:
+            * x, y, w, h: x, y, width, height
+            * spritesheet: a SpriteSheet object
+            * sx, sy: the coords, in multiple of w and h in the spritesheet
+        """
         self.x = x
         self.y = y
         self.w = w
         self.h = h
+
+        self.sx = sx
+        self.sy = sy
+
+        # The element hitbox
+        self.hitbox = Hitbox(x, y, w, h)
+
+        # Sprite sheet rendering
         self.spritesheet = spritesheet
-        self.sprite = spritesheet.render(sx, sy)
 
     def draw(self):
-        pyxel.blt(self.x, self.y, *self.sprite)
+        # Drawn the sprite at the element coords
+        pyxel.blt(self.x, self.y, *self.spritesheet.render(self.sx, self.sy))
+
+    def copy(self):
+        # Return a copy of this element to predict movements
+        return Element(
+                self.x, self.y, self.w, self.h,
+                self.spritesheet, self.sx, self.sy
+        )
+
+    def is_colliding(self, element):
+        # Is this element colliding with element
+        return self.hitbox.x < element.hitbox.x2 and \
+               self.hitbox.x2 > element.hitbox.x and \
+               self.hitbox.y < element.hitbox.y2 and \
+               self.hitbox.y2 > element.hitbox.y
