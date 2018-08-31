03bcf8c1b7e50962ba1e37d90bc7ea88c52e8fd2
Fabien Dupont <fabien.dupont@fullsave.com>
2018-08-31 11:35:10 +0200

    Server class

--- gamejam/server.py
+++ gamejam/server.py
@@ -6,3 +6,33 @@ This file is part of FullSave Gamejam.
 Copyrights 2018 by Fullsave
 """
 
+from .misc import Item
+
+
+class RAM(Item):
+    pass
+
+
+class CPU(Item):
+    pass
+
+
+class Server(Item):
+    def __init__(self, x, y, w, h, spritesheet, sx, sy):
+        Item.__init__(self, x, y, w, h, spritesheet, sx, sy)
+
+        self._ram = 0
+        self._cpu = 0
+
+    def interact(self, Item):
+        """ Interaction with the server
+
+        Arguments:
+            Item: an Item instance (CPU or RAM)
+        """
+        if isinstance(Item, RAM):
+            self._ram = self._ram + 1
+        elif isinstance(Item, CPU):
+            self._cpu = self._cpu + 1
+        else:
+            raise ValueError
