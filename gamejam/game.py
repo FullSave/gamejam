313a9e8fd39cd5bbb82a01df8660c972b70eea69
Fabien Dupont <fabien.dupont@fullsave.com>
2018-08-31 10:07:06 +0200

    Game initialization

--- gamejam/game.py
+++ gamejam/game.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 """
@@ -6,3 +6,29 @@ This file is part of FullSave Gamejam.
 Copyrights 2018 by Fullsave
 """
 
+import os
+
+import pyxel
+
+
+class Game(object):
+    _width = 256
+    _height = 256
+
+    def __init__(self):
+        pyxel.init(self._width, self._height, caption="Gamejam - FullSave")
+
+        assets = os.path.join(os.path.dirname(__file__), 'assets')
+        # pyxel.image(0).load(0, 0, os.path.join(assets, 'logo.png')) 
+
+        pyxel.run(self.update, self.draw)
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+
+if __name__ == '__main__':
+    Game()
