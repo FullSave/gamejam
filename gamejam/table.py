45ad46d96d058afe5ff14e7f48b05d14b3f1bef6
Fabien Dupont <fabien.dupont@fullsave.com>
2018-08-31 11:39:14 +0200

    Table class

--- gamejam/table.py
+++ gamejam/table.py
@@ -6,3 +6,29 @@ This file is part of FullSave Gamejam.
 Copyrights 2018 by Fullsave
 """
 
+from .misc import Element
+from .server import Server
+
+
+class Table(Element):
+    def __init__(self, x, y, w, h, spritesheet, sx, sy):
+        Element.__init_(self, x, y, w, h, spritesheet, sx, sy)
+
+        self._server = None
+
+    def interact(self, item):
+        """ Interaction with the table
+
+        Arguments:
+            item : an item
+
+        If the table has a server on it, the item is interacted with it
+        """
+        if isinstance(item, Server):
+            if self._server is not None:
+                raise ValueError
+
+            self._server = item
+
+        else:
+            self._server.interact(item)
