eff9d3cbe91ac5e72f72a02d7f329470d96faabc
Yamine Bulach <yamine.bulach@fullsave.com>
2018-10-23 23:04:24 +0200

    Modified player hitbox

--- gamejam/player.py
+++ gamejam/player.py
@@ -20,7 +20,7 @@ class Player(Element):
     frames = 4
 
     def __init__(self, map_, x, y):
-        Element.__init__(self, x, y, 32, 32, Hitbox(8, 16, 16, 13))
+        Element.__init__(self, x, y, 32, 32, Hitbox(7, 16, 18, 15))
 
         # Carried item
         self._item = None
