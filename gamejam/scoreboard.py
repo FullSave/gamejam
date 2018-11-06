2998ad0dcf34aa5601a53fede3778f0c4e1e46db
Fabien Dupont <fabien.dupont@fullsave.com>
2018-11-06 11:51:43 +0100

    Scoreboard texture

--- gamejam/scoreboard.py
+++ gamejam/scoreboard.py
@@ -11,7 +11,7 @@ import random
 from pyxel.constants import FONT_WIDTH, FONT_HEIGHT
 from .server import Server, RAM, CPU
 from .rack import Rack
-
+from .misc import SpriteSheet
 
 TXT_COLOR = 7
 TXT_SPACE = 2
@@ -50,6 +50,8 @@ class Order(object):
     def draw(self, x, y):
         #BG
         pyxel.rect(x+2, y+2, x+52, y+19, 1)
+        pyxel.rectb(x+2, y+2, x+52, y+19, 5)
+
 
         #rack no
         pyxel.rect(x+4, y+4, x+12, y+10, 10)
@@ -82,6 +84,7 @@ class ScoreBoard(object):
         self._map = map
         self._margin = Margin(5, 5, 5, 5)
         self._orders = []
+        self._bg_texture = SpriteSheet().get_sprite("wall_texture")
 
         for i in range(0, 3):
             self._orders.append(self.generate_order())
@@ -125,17 +128,23 @@ class ScoreBoard(object):
     def get_timer(self):
         return self._map.time
 
-    def _draw_borders(self, x, y, w, h):
-        pyxel.line(x, y+h-1, x+w, y+h-1, BORDER_COLOR)
-        pyxel.line(x, y+h-2, x+w, y+h-2, BORDER_COLOR)
+    def _draw_bg(self, x, y):
+        offset_x = 0
+        while offset_x < 224:
+            pyxel.blt(x + offset_x, y, *self._bg_texture.render())
+            offset_x += 32
+        # XXX pyxel.line(x, y+h-1, x+w, y+h-1, BORDER_COLOR)
+        # XXX pyxel.line(x, y+h-2, x+w, y+h-2, BORDER_COLOR)
 
     def _draw_score(self, x, y):
         margin = self.get_margin()
         x += margin.left
         y += margin.top
-        pyxel.text(x, y, "TIME: {:.1f}".format(self.get_timer()), TXT_COLOR)
+        pyxel.rect(2, 2, 52, 19, 0)
+        pyxel.rectb(2, 2, 52, 19, 5)
+        pyxel.text(x-1, y-1, "TIME: {:.1f}".format(self.get_timer()), 11)
         y += FONT_HEIGHT + TXT_SPACE
-        pyxel.text(x, y, "SCORE: {:d}".format(self.get_score()), TXT_COLOR)
+        pyxel.text(x-1, y-1, "SCORE: {:d}".format(self.get_score()), 11)
 
     def _draw_order(self, x, y, order=None):
         if order:
@@ -154,6 +163,6 @@ class ScoreBoard(object):
     def draw(self):
         (x, y) = self.get_pos()
         (w, h) = self.get_size()
-        self._draw_borders(x, y, w, h)
+        self._draw_bg(x, y)
         self._draw_score(x, y)
         self._draw_orders(56 + x, y)
